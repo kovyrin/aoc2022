f94d685fc717563dc6c08b7c936679fc950bc2d3
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2023-05-08 12:16:45 -0400

    Implement search by multiple keywords (or/and)

--- search-index-problem/src/main.rs
+++ search-index-problem/src/main.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 const DATA_FILE: &str = "products.json";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq)]
 struct Product {
   id: i32,
   name: String,
@@ -39,6 +39,9 @@ fn load_products(filename: &str) -> Vec<Product> {
   return serde_json::from_str(&data).unwrap();
 }
 
+type SearchIndex<'a> = HashMap<String, Vec<&'a Product>>;
+type SearchResults<'a> = Vec<&'a Product>;
+
 fn main() {
   // load the data file
   let products = load_products(DATA_FILE);
@@ -55,9 +58,17 @@ fn main() {
 
   let bananas = find_by_keyword(&word_index, "banana");
   print_results("Found products with keyword 'banana':", &bananas);
+
+  let keywords = Vec::from(["cool", "keyboard"]);
+  let cool_keyboards = find_by_keywords_union(&word_index, keywords);
+  print_results("Found products with keywords 'cool OR keyboard':", &cool_keyboards);
+
+  let keywords = Vec::from(["cool", "keyboard"]);
+  let cool_keyboards = find_by_keywords_intersect(&word_index, keywords);
+  print_results("Found products with keywords 'cool AND keyboard':", &cool_keyboards);
 }
 
-fn print_results(title: &str, products: &Vec<&Product>) {
+fn print_results(title: &str, products: &SearchResults) {
   println!("\n{}", title);
   if products.is_empty() {
     println!("No results found");
@@ -69,7 +80,33 @@ fn print_results(title: &str, products: &Vec<&Product>) {
   }
 }
 
-fn find_by_keyword<'a>(word_index: &'a HashMap<String, Vec<&Product>>, keyword: &str) -> Vec<&'a Product> {
+fn find_by_keywords_intersect<'a>(word_index: &'a SearchIndex, keywords: Vec<&str>) -> SearchResults<'a> {
+  let mut results: SearchResults = Vec::new();
+  for keyword in keywords {
+    let keyword_results = find_by_keyword(word_index, keyword);
+    if results.is_empty() {
+      results = keyword_results;
+      continue;
+    }
+    results = results.into_iter().filter(|product| keyword_results.contains(product)).collect();
+  }
+  return results;
+}
+
+fn find_by_keywords_union<'a>(word_index: &'a SearchIndex, keywords: Vec<&str>) -> SearchResults<'a> {
+  let mut results: SearchResults = Vec::new();
+  for keyword in keywords {
+    let keyword_results = find_by_keyword(word_index, keyword);
+    for keyword_result in keyword_results {
+      if !results.contains(&keyword_result) {
+        results.push(keyword_result);
+      }
+    }
+  }
+  return results;
+}
+
+fn find_by_keyword<'a>(word_index: &'a SearchIndex, keyword: &str) -> SearchResults<'a> {
   let keyword = keyword.to_string().to_lowercase();
   if !word_index.contains_key(&keyword) {
     return Vec::new();
