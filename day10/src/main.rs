f7185cce34122310ac975ff2fc1659afc029f30f
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2023-05-17 10:23:43 -0400

    day10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -2,33 +2,63 @@ use std::{fs::read_to_string, str::Lines};
 use anyhow::{Result, Context};
 
 #[derive(Debug)]
-struct Cpu {
-    register: i64,
-    register_values: Vec<i64>
+struct Computer {
+    sprite_center: i64,
+    pixel_pos: usize,
+    crt: Vec<bool>
 }
 
-impl Cpu {
+const WIDTH: usize = 40;
+const HEIGHT: usize = 6;
+
+impl Computer {
     fn new() -> Self {
-        Cpu {
-            register: 1,
-            register_values: vec![1]
-        }
+        let mut c = Computer {
+            sprite_center: 1,
+            pixel_pos: 0,
+            crt: Vec::with_capacity(WIDTH * HEIGHT)
+        };
+        c.crt.resize(WIDTH * HEIGHT, false);
+        c
     }
 
     fn execute_command(&mut self, command: &str) {
         if command == "noop" {
-            self.register_values.push(self.register);
+            self.color_pixel();
             return;
         }
 
         if command.starts_with("addx") {
-            let change: i64 = command.split_whitespace().
-                nth(1).expect("get addx arg").
-                parse().expect("parse addx arg");
-            self.register_values.push(self.register);
-            self.register_values.push(self.register);
-            self.register += change
+            let change: i64 = command.split_whitespace()
+                .nth(1).expect("get addx arg")
+                .parse().expect("parse addx arg");
+            self.color_pixel();
+            self.color_pixel();
+            self.sprite_center += change
         }
+    }
+
+    fn pixel_char(&self, value: bool) -> char {
+        if value { '#' } else { ' ' }
+    }
+
+    fn color_pixel(&mut self) {
+        let sprite = self.sprite_center-1..=self.sprite_center+1;
+        let pixel_pos = (self.pixel_pos % WIDTH) as i64;
+        let pixel = sprite.contains(&pixel_pos);
+        self.crt[self.pixel_pos] = pixel;
+        self.pixel_pos += 1;
+    }
+
+    fn print_screen(&self) {
+        for row in 0..HEIGHT {
+            for col in 0..WIDTH {
+                let pos = row*WIDTH + col;
+                print!("{}", self.pixel_char(self.crt[pos]))
+            }
+            println!()
+        }
+        println!();
     }
 }
 
@@ -45,16 +75,13 @@ fn main() -> Result<()>{
     let input: String = read_to_string(input_file).context("failed to read the data file")?;
     let lines: Lines = input.lines();
 
-    let mut cpu = Cpu::new();
+    let mut computer = Computer::new();
 
     for command in lines {
-        cpu.execute_command(command)
+        computer.execute_command(command)
     }
 
-    let mut strength = 0;
-    for step in vec![20, 60, 100, 140, 180, 220] {
-        strength += cpu.register_values[step] * step as i64
-    }
-    println!("Strength: {}", strength);
+    computer.print_screen();
+
     Ok(())
 }
