5eafa129570c928c6e1cf7d475e4f6a43b8edbbb
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2023-05-18 16:55:57 -0400

    day13 part1

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,14 +1,28 @@
-use std::{fs::read_to_string, str::{Lines, Chars}, iter::Peekable};
+use std::{fs::read_to_string, str::{Lines, Chars}, iter::Peekable, cmp::Ordering, fmt::Write};
 use anyhow::{Context, Result};
 
 #[derive(Debug)]
-enum List {
+enum Packet {
     Single(usize),
-    List(Vec<List>)
+    List(Vec<Packet>)
 }
 
-impl List {
-    fn list_from_str(line: &mut Peekable<Chars>) -> List {
+impl std::fmt::Display for Packet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Packet::List(items) => {
+                f.write_char('[')?;
+                let str = items.iter().map(|i| format!("{i}")).collect::<Vec<String>>().join("");
+                write!(f, "{str}")?;
+                f.write_char(']')
+            },
+            Packet::Single(item) => write!(f, "{}", item),
+        }
+    }
+}
+
+impl Packet {
+    fn list_from_str(line: &mut Peekable<Chars>) -> Packet {
         let mut list_vals = Vec::default();
         while let Some(c) = line.peek() {
             match c {
@@ -30,12 +44,21 @@ impl List {
                 }
             }
         }
-        List::List(list_vals)
+        Packet::List(list_vals)
     }
 
-    fn number_from_str(line: &mut Peekable<Chars>) -> List {
+    fn number_from_str(line: &mut Peekable<Chars>) -> Packet {
         let value: String = line.by_ref().take_while(|c| c.is_digit(10)).collect();
-        List::Single(value.parse().expect("int parsing"))
+        Packet::Single(value.parse().expect("int parsing"))
+    }
+
+    fn smaller_than(&self, other: &Packet) -> bool {
+        match (self, other) {
+            (Packet::List(right), Packet::List(left)) => {
+                self::compare_lists(right, left).is_lt()
+            },
+            _ => false
+        }
     }
 }
 
@@ -52,19 +75,72 @@ fn main() -> Result<()>{
 
     let input: String = read_to_string(input_file).context("failed to read the data file")?;
     let mut lines: Lines = input.lines();
+    let mut pair_index = 1;
+    let mut index_sum = 0;
 
     loop {
+        println!("====== Pair {pair_index} ======");
+
         let line1 = lines.next().expect("reading line 1");
         let line2 = lines.next().expect("reading line 2");
+        println!("- {}", line1);
+        println!("- {}", line2);
 
-        let line1 = List::list_from_str(&mut line1.chars().peekable());
-        let line2 = List::list_from_str(&mut line2.chars().peekable());
+        let line1 = Packet::list_from_str(&mut line1.chars().peekable());
+        let line2 = Packet::list_from_str(&mut line2.chars().peekable());
 
-        println!("Line 1: {:?}", line1);
-        println!("List 2: {:?}", line2);
+
+        if line1.smaller_than(&line2) {
+            println!("+ Ordering is correct");
+            index_sum += pair_index;
+        } else {
+            println!("- Ordering is incorrect");
+        }
 
         if let None = lines.next() { break }
+        pair_index += 1;
+        println!();
     }
 
+    println!("Sum of list indexes that are in correct order: {index_sum}");
     Ok(())
+}
+
+fn compare_one_item(left: &Packet, right: &Packet) -> Ordering {
+    println!("Compare {} vs {}", left, right);
+    match (left, right) {
+        (Packet::Single(left), Packet::Single(right)) => {
+            left.cmp(right)
+        },
+        (Packet::List(left), Packet::List(right)) => {
+            return compare_lists(left, right)
+        },
+        (Packet::List(_), Packet::Single(right)) => {
+            let right = Packet::List(vec![Packet::Single(*right)]);
+            return compare_one_item(left, &right)
+        },
+        (Packet::Single(left), Packet::List(_)) => {
+            let left = Packet::List(vec![Packet::Single(*left)]);
+            return compare_one_item(&left, right)
+        },
+    }
+}
+
+fn compare_lists(left: &Vec<Packet>, right: &Vec<Packet>) -> Ordering {
+    for pos in 0..left.len() {
+        if pos >= right.len() {
+            println!("* Right ran out of items");
+            return Ordering::Greater
+        }
+        let left = &left[pos];
+        let right = &right[pos];
+        let res = compare_one_item(left, right);
+        if !res.is_eq() { return res }
+    }
+    if left.len() == right.len() {
+        return Ordering::Equal
+    } else {
+        println!("* Left ran out of items");
+        Ordering::Less
+    }
 }
