371a6319c3ad2139cb0c1949767a4345f4d27d93
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2023-05-19 00:07:15 -0400

    Cleanup

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -25,26 +25,26 @@ impl Map {
         }
     }
 
-    fn draw_lines(&mut self, rock_line: &[Point]) {
+    fn draw_rock_paths(&mut self, rock_line: &[Point]) {
         let mut segment_start = &rock_line[0];
         for i in 1..rock_line.len() {
             let segment_end = &rock_line[i];
-            self.draw_line(segment_start, segment_end);
+            self.draw_rock_path(segment_start, segment_end);
             segment_start = segment_end;
         }
     }
 
-    fn draw_line(&mut self, segment_start: &Point, segment_end: &Point) {
+    fn draw_rock_path(&mut self, segment_start: &Point, segment_end: &Point) {
         if segment_start.x == segment_end.x {
-            self.draw_vertical_line(segment_start, segment_end);
+            self.draw_vertical_path(segment_start, segment_end);
         } else if segment_start.y == segment_end.y {
-            self.draw_horizontal_line(segment_start, segment_end);
+            self.draw_horizontal_path(segment_start, segment_end);
         } else {
             panic!("Unknown type of line: {:?} to {:?}", segment_start, segment_end);
         }
     }
 
-    fn draw_vertical_line(&mut self, segment_start: &Point, segment_end: &Point) {
+    fn draw_vertical_path(&mut self, segment_start: &Point, segment_end: &Point) {
         let start_y = min(segment_start.y, segment_end.y);
         let end_y = max(segment_start.y, segment_end.y);
         for y in start_y..=end_y {
@@ -52,7 +52,7 @@ impl Map {
         }
     }
 
-    fn draw_horizontal_line(&mut self, segment_start: &Point, segment_end: &Point) {
+    fn draw_horizontal_path(&mut self, segment_start: &Point, segment_end: &Point) {
         let start_x = min(segment_start.x, segment_end.x);
         let end_x = max(segment_start.x, segment_end.x);
         for x in start_x..=end_x {
@@ -100,14 +100,14 @@ fn main() -> Result<()>{
     let input: String = read_to_string(input_file).context("failed to read the data file")?;
     let lines: Lines = input.lines();
 
-    let mut rock_lines: Vec<Vec<Point>> = Vec::default();
+    let mut rock_paths: Vec<Vec<Point>> = Vec::default();
 
     let mut map_width: usize = 0;
     let mut map_height: usize = 0;
 
     for line in lines {
         let line = line.replace(" -> ", ">");
-        let points: Vec<Point> = line.split(">").map(|coords| {
+        let rock_path: Vec<Point> = line.split(">").map(|coords| {
             let mut coords = coords.split(",").map(|c| c.parse::<usize>());
 
             let x = coords.next().expect("parse x").unwrap();
@@ -119,12 +119,12 @@ fn main() -> Result<()>{
             Point { x, y }
         }).collect();
 
-        rock_lines.push(points);
+        rock_paths.push(rock_path);
     }
 
     let mut map = Map::new(map_width + 1, map_height + 1);
-    for rock_line in rock_lines.iter() {
-        map.draw_lines(rock_line);
+    for rock_line in rock_paths.iter() {
+        map.draw_rock_paths(rock_line);
     }
 
     let mut sand_count = 0;
